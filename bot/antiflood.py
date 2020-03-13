e171c90f6ee5fcadf7e91d9b243ce746336bd665
Davide Riva <driva95@protonmail.com>
2020-03-13 02:43:56 +0100

    Bugflix on exceptions order

--- bot/antiflood.py
+++ bot/antiflood.py
@@ -8,10 +8,10 @@ async def send_message(bot: Bot, chat_id: int, message: str):
         await bot.send_message(chat_id, message)
     except (exceptions.BotBlocked, exceptions.UserDeactivated, exceptions.ChatNotFound) as e:
         logging.debug(f'Target [ID:{chat_id}]: Impossible to reach him.')
-    except exceptions.TelegramAPIError as e:
-        logging.exception(f'Target [ID:{chat_id}]: Failed.')
     except exceptions.RetryAfter as e:
         logging.warning(
             f'Target [ID:{chat_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds.')
         await asyncio.sleep(e.timeout)
         await send_message(bot, chat_id, message)
+    except exceptions.TelegramAPIError as e:
+        logging.exception(f'Target [ID:{chat_id}]: Failed.')
