be1a7b0b6697dabed91d97659f344d50b6f39eb8
Davide Riva <driva95@protonmail.com>
2020-03-18 11:48:48 +0100

    Reverted to the old way of adding constraints

--- bot/utils/dbmigration.py
+++ bot/utils/dbmigration.py
@@ -2,7 +2,7 @@ from neo4j import Driver, BoltStatementResult
 
 
 def migrate(db: Driver):
-    constraints_to_keep = ['chatID', 'referrer']
+    constraints_to_keep = []
 
     # Get constraints on the DB
     with db.session() as session:
@@ -13,14 +13,25 @@ def migrate(db: Driver):
         all_constraints = db_constraints.value()
 
     # Create new constraints
-    for constraint_to_keep in constraints_to_keep:
-        if constraint_to_keep not in all_constraints:
-            with db.session() as session:
-                session.run(f"""
-                            CREATE CONSTRAINT {constraint_to_keep}
-                            ON (n:Person)
-                            ASSERT n.chatID IS UNIQUE
-                            """)
+    constraint_to_keep = 'chatID'
+    if constraint_to_keep not in all_constraints:
+        with db.session() as session:
+            session.run(f"""
+                        CREATE CONSTRAINT {constraint_to_keep}
+                        ON (n:Person)
+                        ASSERT n.chatID IS UNIQUE
+                        """)
+    constraints_to_keep.append(constraint_to_keep)
+
+    constraint_to_keep = 'referrer'
+    if constraint_to_keep not in all_constraints:
+        with db.session() as session:
+            session.run(f"""
+                        CREATE CONSTRAINT {constraint_to_keep}
+                        ON (n:Person)
+                        ASSERT n.referrer IS UNIQUE
+                        """)
+    constraints_to_keep.append(constraint_to_keep)
 
     # Drop unused ones
     db_constraints_to_drop = set(all_constraints) - set(constraints_to_keep)
