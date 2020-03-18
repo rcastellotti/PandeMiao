ff6bf0181dbcadbb020a18e922ae108cd6f9bc6a
Davide Riva <driva95@protonmail.com>
2020-03-18 02:29:43 +0100

    Contraints to add are now in a list

--- bot/utils/dbmigration.py
+++ bot/utils/dbmigration.py
@@ -1,31 +1,24 @@
-from neo4j import Driver, Record, BoltStatementResult
+from neo4j import Driver, BoltStatementResult
 
 def migrate(db: Driver):
-    constraints_to_keep = []
+    constraints_to_keep = ['chatID', 'referrer']
+    
     with db.session() as session:
-        db_constraints:BoltStatementResult = session.run("""
+        db_constraints: BoltStatementResult = session.run("""
                                      CALL db.constraints
                                      YIELD name
                                      """)
         all_constraints = db_constraints.value()
-
-    if 'chatID' not in all_constraints:
-        with db.session() as session:
-            session.run("""
-                        CREATE CONSTRAINT chatID
-                        ON (n:Person)
-                        ASSERT n.chatID IS UNIQUE
-                        """)
-    constraints_to_keep.append('chatID')
-
-    if 'referrer' not in all_constraints:
-        with db.session() as session:
-            session.run("""
-                        CREATE CONSTRAINT referrer
-                        ON (n:Person)
-                        ASSERT n.referrer IS UNIQUE
-                        """)
-    constraints_to_keep.append('referrer')
+    
+    
+    for constraint_to_keep in constraints_to_keep:
+        if constraint_to_keep not in all_constraints:
+            with db.session() as session:
+                session.run(f"""
+                            CREATE CONSTRAINT {constraint_to_keep}
+                            ON (n:Person)
+                            ASSERT n.chatID IS UNIQUE
+                            """)
 
     db_constraints_to_drop = set(all_constraints) - set(constraints_to_keep)
     for constraint_name in db_constraints_to_drop:
