94762aa5fbfc7372869353c86532f66db4215552
Davide Riva <driva95@protonmail.com>
2020-03-21 15:34:38 +0100

    Antiflood module removed

--- bot/notify.py
+++ bot/notify.py
@@ -1,16 +1,29 @@
 import settings
-import antiflood
 import asyncio
-from aiogram import Bot, Dispatcher, executor
+import logging
+from aiogram import Bot, Dispatcher, executor, exceptions
 
 
 async def daily_update() -> None:
-    # Notify all users
+    # TODO: Notify all users
     for i in range(2):
+        chat_id = 844457200
         message = 'meow ' + str(i)
-        await antiflood.send_message(message, 844457200,
-                                     bot, silent=True)
+        await send_message(chat_id, message)
+
+
+async def send_message(chat_id: int, message: str) -> None:
+    try:
+        await bot.send_message(chat_id=chat_id, text=message, disable_notification=True)
         await asyncio.sleep(1.0)  # Max 1 message / sec
+
+    except (exceptions.BotBlocked,
+            exceptions.UserDeactivated,
+            exceptions.ChatNotFound):
+        logging.debug(f'Target [ID:{chat_id}]: Impossible to reach him.')
+
+    except exceptions.TelegramAPIError:
+        logging.exception(f'Target [ID:{chat_id}]: Failed.')
 
 
 # Initialize bot
