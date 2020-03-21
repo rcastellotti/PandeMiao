6b4dfdefec3f6eda83848d331e66bcb9f6ea1c75
Davide Riva <driva95@protonmail.com>
2020-03-21 15:24:48 +0100

    Silent checks implemented

--- bot/antiflood.py
+++ bot/antiflood.py
@@ -6,19 +6,12 @@ from aiogram import Bot, exceptions
 async def send_message(message: str, chat_id: int,
                        bot: Bot, silent: bool = False) -> None:
     try:
-        await bot.send_message(chat_id, message)
+        await bot.send_message(chat_id, message, disable_notification=silent)
 
     except (exceptions.BotBlocked,
             exceptions.UserDeactivated,
             exceptions.ChatNotFound):
         logging.debug(f'Target [ID:{chat_id}]: Impossible to reach him.')
-
-    except exceptions.RetryAfter as e:
-        logging.warning(f'Target [ID:{chat_id}]: '
-                        'Flood limit is exceeded. '
-                        'Sleep {e.timeout} seconds.')
-        await asyncio.sleep(e.timeout)
-        await send_message(bot, chat_id, message)
 
     except exceptions.TelegramAPIError:
         logging.exception(f'Target [ID:{chat_id}]: Failed.')
