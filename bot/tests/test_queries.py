074794792dabc964a70c6452e3cb7f81fd6568a3
Davide Riva <driva95@protonmail.com>
2020-03-26 23:05:48 +0100

    Methods for testing now separated by class

--- bot/tests/test_queries.py
+++ bot/tests/test_queries.py
@@ -1,83 +1,25 @@
 '''Test module utils.queries.'''
 
-# pylint: disable=C0116
-# pylint: disable=C0115
-
 import unittest
 from neo4j import Driver
 from utils.dbinit import db_connect
-import utils.transactions
 import utils.queries
 
 
-class TestQueries(unittest.TestCase):
+class TestQuery(unittest.TestCase):
+    '''Base class for testing a query.'''
+
+    __test__ = False  # Don't have to be run
     db_link: Driver
 
     def setUp(self) -> None:
+        '''Connect to the DB.'''
+
         self.db_link = db_connect(retry=5, wait_sec=5)
 
-    def test_set_infected(self) -> None:
-        # Creation of a node
-        referrer = utils.queries.set_infected(self.db_link, 0)
-        self.assertIsNotNone(referrer)
-
-        # If a node exist, just return it
-        referrer_eq = utils.queries.set_infected(self.db_link, 0)
-        self.assertIsNotNone(referrer_eq)
-        self.assertEqual(referrer, referrer_eq)
-
-        # Different nodes have different referrer links
-        referrer_diff = utils.queries.set_infected(self.db_link, 1)
-        self.assertIsNotNone(referrer_diff)
-        self.assertNotEqual(referrer, referrer_diff)
-
-    def test_set_infected_from(self) -> None:
-        # Creation of the patient zero
-        patient_zero_referrer = utils.queries.set_infected(self.db_link, 0)
-        self.assertIsNotNone(patient_zero_referrer)
-
-        # Patient zero -> patient 1
-        referred_infected = utils.queries.set_infected_from(
-            self.db_link, 1, patient_zero_referrer)
-        self.assertIsNotNone(referred_infected)
-        self.assertNotEqual(patient_zero_referrer, referred_infected)
-
-        # If a node exist, just return it
-        referred_eq = utils.queries.set_infected_from(
-            self.db_link, 1, 'no_matter_the_referral')
-        self.assertIsNotNone(referred_eq)
-        self.assertEqual(referred_infected, referred_eq)
-
-        # If the referral is wrong, create a patient zero
-        wrong_referral = utils.queries.set_infected_from(
-            self.db_link, 2, 'wrong_referral')
-        self.assertIsNotNone(wrong_referral)
-
-        # If a node exist and we try to re-infect it, nothing happens
-        wrong_linked = utils.queries.set_infected_from(
-            self.db_link, 2, patient_zero_referrer)
-        self.assertIsNotNone(wrong_linked)
-        self.assertEqual(wrong_referral, wrong_linked)
-
-        # If a node exist and we try to re-infect it,
-        # it souldn't change the infector
-        ref_init = utils.queries.set_infected(self.db_link, 3)
-        ref_post = utils.queries.set_infected(self.db_link, 4)
-        utils.queries.set_infected_from(self.db_link, 5, ref_init)
-        utils.queries.set_infected_from(self.db_link, 5, ref_post)
-        with self.db_link.session() as session:
-            result = session.read_transaction(
-                utils.transactions.get_infector, 5)
-        self.assertEqual(result, 3)
-        self.assertNotEqual(result, 4)
-
-        # If a node exist, it is a patient zero
-        # and we try to infect it, nothing happens
-        no_referred = utils.queries.set_infected(self.db_link, 1)
-        self.assertIsNotNone(no_referred)
-        self.assertEqual(referred_infected, no_referred)
-
     def tearDown(self) -> None:
+        '''Delete all data from the DB.'''
+
         with self.db_link.session() as session:
             # Clean the DB
             session.run("""
@@ -87,5 +29,118 @@ class TestQueries(unittest.TestCase):
         self.db_link.close()
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestSetInfectedFrom(TestQuery):
+    '''Test the utils.queries.set_infected method.'''
+
+    __test__ = True
+
+    def test_new_node_referrer(self) -> None:
+        '''Test creation of a new node.'''
+
+        referrer = utils.queries.set_infected(self.db_link, 0)
+        self.assertIsNotNone(referrer)
+
+    def test_recreate_node(self) -> None:
+        '''Test re-creation of a node.'''
+
+        old_ref = utils.queries.set_infected(self.db_link, 0)
+        new_ref = utils.queries.set_infected(self.db_link, 0)
+        self.assertEqual(new_ref, old_ref)
+
+    def test_unique_referrer(self) -> None:
+        '''Test uniqueness of referrals.'''
+
+        old_ref = utils.queries.set_infected(self.db_link, 0)
+        self.assertIsNotNone(old_ref)
+
+        new_ref = utils.queries.set_infected(self.db_link, 1)
+        self.assertIsNotNone(new_ref)
+
+        self.assertNotEqual(new_ref, old_ref)
+
+
+class TestSetInfected(TestQuery):
+    '''Test the utils.queries.set_infected_from method.'''
+
+    __test__ = True
+    patient0_chat_id = 0
+    patient0_ref: str
+
+    def setUp(self) -> None:
+        '''Create a patient zero.'''
+
+        super().setUp()
+        self.patient0_ref = utils.queries.set_infected(
+            self.db_link, self.patient0_chat_id)
+
+    def test_new_node_referrer(self) -> None:
+        '''Test creation of a new node.'''
+
+        infected_ref = utils.queries.set_infected_from(
+            self.db_link, 1, self.patient0_ref)
+        self.assertIsNotNone(infected_ref)
+        self.assertNotEqual(infected_ref, self.patient0_ref)
+
+    def test_recreate_node(self) -> None:
+        '''Test re-creation of a node.'''
+
+        # If they are both infected from someone
+        old_ref = utils.queries.set_infected_from(
+            self.db_link, 1, self.patient0_ref)
+        new_ref = utils.queries.set_infected_from(
+            self.db_link, 1, self.patient0_ref)
+        self.assertEqual(new_ref, old_ref)
+
+        # If the first is a patient zero
+        old_ref = utils.queries.set_infected(self.db_link, 1)
+        new_ref = utils.queries.set_infected_from(
+            self.db_link, 1, self.patient0_ref)
+        self.assertEqual(new_ref, old_ref)
+
+        # If the second is a patient zero
+        old_ref = utils.queries.set_infected_from(
+            self.db_link, 1, self.patient0_ref)
+        new_ref = utils.queries.set_infected(self.db_link, 1)
+        self.assertEqual(new_ref, old_ref)
+
+    def test_unique_referrer(self) -> None:
+        '''Test uniqueness of referrals.'''
+
+        old_ref = utils.queries.set_infected_from(
+            self.db_link, 1, self.patient0_ref)
+        self.assertIsNotNone(old_ref)
+
+        new_ref = utils.queries.set_infected_from(
+            self.db_link, 2, self.patient0_ref)
+        self.assertIsNotNone(new_ref)
+        self.assertNotEqual(new_ref, old_ref, self.patient0_ref)
+
+    def test_wrong_referrer(self) -> None:
+        '''Test when the referrer is wrong.'''
+
+        # If the referral is wrong, create a patient zero
+        referrer = utils.queries.set_infected_from(
+            self.db_link, 1, 'wrong_referral')
+        self.assertIsNotNone(referrer)
+
+        with self.db_link.session() as session:
+            # It shouldn't have an infector
+            self.assertRaises(AttributeError, session.read_transaction,
+                              utils.transactions.get_infector, 1)
+
+    def change_referrer(self) -> None:
+        '''Test if the infector change after re-creation of a node'''
+        # If a node exist and we try to re-infect it,
+        # it shouldn't change the infector
+
+        ref_init = utils.queries.set_infected(self.db_link, 1)
+        utils.queries.set_infected_from(self.db_link, 3, ref_init)
+
+        ref_post = utils.queries.set_infected(self.db_link, 2)
+        utils.queries.set_infected_from(self.db_link, 3, ref_post)
+
+        with self.db_link.session() as session:
+            result = session.read_transaction(
+                utils.transactions.get_infector, 3)
+        self.assertEqual(result, 1)
+        self.assertNotEqual(result, 2)
